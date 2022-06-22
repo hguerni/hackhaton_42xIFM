e7a3fc4d3815b74936c34d80da2202b2f10621fd
cmenasse <cmenasse@student.42.fr>
2022-06-22 12:08:14 +0200

    router

---
+++ front/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <div>
+            <h1> Pixel Wardrobe</h1>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
