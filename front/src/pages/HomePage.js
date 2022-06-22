b8f720a2b21855a46564c689c27281a4ea3a1b97
cmenasse <cmenasse@student.42.fr>
2022-06-22 12:26:42 +0200

    landing

--- front/src/pages/HomePage.js
+++ front/src/pages/HomePage.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 const HomePage = () => {
   return (
     <div>
         <div>
-            <h1> Pixel Wardrobe</h1>
+            <h1>Pixel Wardrobe</h1>
+            <p>Fashion in the Metaverse</p>
+        </div>
+        <div className='buttons-homepage'>
+                <button>
+                    <Link to='/' className='link'>Log in</Link>
+                </button>
+                <button>
+                    <Link to='/' className='link'>Sign up</Link>
+                </button>
+                <button>
+                    <Link to='/' className='link'>Demo</Link>
+                </button>            
         </div>
     </div>
   )
